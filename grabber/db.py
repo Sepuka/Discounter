08ca8ec8072cc14ac69c0e6fd852ca17f688159a
Maksim Shlomin <maksim.shlomin@gmail.com>
2012-03-17 19:11:03 +0400

    Создан класс для работы с БД и добавлены настройки в конфиг

--- grabber/db.py
+++ grabber/db.py
@@ -1,3 +1,58 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 #vim: set syntax=python
+
+from config import Config
+from logger import Logger
+import MySQLdb
+import sys
+
+class DB(object):
+    _conf   = None
+    _log    = None
+    _conn   = None
+    _cursor = None
+
+    def __init__(self):
+        self._conf = Config()
+        self._log = Logger()
+        self._connect()
+
+    def __del__(self):
+        if isinstance(self._conn, MySQLdb.connections.Connection):
+            self._conn.commit()
+            self._cursor.close()
+            self._conn.close()
+
+    def _connect(self):
+        if self._conn is None:
+            try:
+                self._conn = MySQLdb.connect(
+                    host = self._conf.get('db', 'host'),
+                    user = self._conf.get('db', 'user'),
+                    passwd = self._conf.get('db', 'pass'),
+                    db = self._conf.get('db', 'db'),
+                    charset = self._conf.get('db', 'encode')
+                )
+                self._cursor = self._conn.cursor()
+            except Exception, ex:
+                self._log.critical('Ошибка соединения с БД', exc_info=True)
+                sys.exit(ex)
+            else:
+                self._log.debug('Создано соединение с БД')
+
+    @property
+    def cursor(self):
+        return self._cursor
+
+    def execute(self, query, *params, **kw):
+        try:
+            self._cursor.execute(query, params)
+            if self._cursor._warnings:
+                self._log.critical('warning detected!')
+                sys.exit('Warning detected!')
+        except (MySQLdb.Error, MySQLdb.IntegrityError), e:
+            self._log.critical('Mysql runtime error %d: %s', e.args[0], e.args[1])
+            sys.exit(e.args[1])
+        else:
+            return self._cursor.lastrowid
--- grabber/grabber.py
+++ grabber/grabber.py
@@ -4,15 +4,35 @@
 
 from config import Config
 from logger import Logger
+from db import DB
+
+#TODO Написать перехватчик ошибок (как он там называется)
 
 class Grabber(object):
     _conf   = None
     _log    = None
+    _db     = None
 
     def __init__(self, configPath):
         self._conf = Config(configPath)
         self._log = Logger()
+        self._db = DB()
         self._log.info('Запущен discounter')
+
+        modules = self._getGrabModules()
+        self._log.info('Предстоит выполнить %d модулей', len(modules))
+
+    def _getGrabModules(self):
+        """
+        Получение кортежа модулей для работы
+        @return tuple
+        """
+        query = """SELECT `module` FROM `resource` WHERE `enabled`='1'"""
+        self._db.execute(query)
+        if self._db.cursor.rowcount:
+            return self._db.cursor.fetchone()
+        else:
+            return tuple()
 
 if __name__ == '__main__':
     grabber = Grabber('/srv/www/Discounter/config.cfg')
